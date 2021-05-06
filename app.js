44edbfd3d17559d8d33a1e72c8ece9ae1611ea38
Juan Marques <juan.marques@docshifter.com>
2021-05-06 23:14:19 +0200

    Fixing user auth and adding multi select imgs

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 /* Feed */
- 
+
+const novoUsuario = 1;
+const usuarioCadastrado = 2;
+
 function exibirModal() {
     let modal = document.getElementById("shareEntryModal")
     modal.style.display = "block"
@@ -14,15 +17,16 @@ function fecharModal() {
     document.body.style.overflow = "auto" // exibir a barra de scroll quando fechamos a modal
 }
 
-function publicarPost(){
-    let elementoPost = document.getElementById("entrada-de-dados") //estou colocando a div "entrada-de-dados" do html dentro da const elemento Post
-    let conteudoPost =  elementoPost.innerText // acessando o texto da div do modal post    
+function publicarPost() {
+    const elementoPost = document.getElementById("entrada-de-dados") //estou colocando a div "entrada-de-dados" do html dentro da const elemento Post
+    const conteudoPost = elementoPost.innerText // acessando o texto da div do modal post
+    let imgPostModal = document.getElementById("img-modal-post")
     const recuperarSessao = document.getElementById("sessao-de-post") // estou colocando a sessao de post do html dentro da const recuperarSessao
-    
+
     // criando a div principal(container)
     const criandoDiv = document.createElement("div") // div principar, div container
     criandoDiv.className = "post container" // div que criamos no js herdar o estilo css que criamos na div do html
-   
+
     // criando a div que amarra nome do usuario e foto
     const divInformacaoDoUsuario = document.createElement("div")// criando div que amarra foto de perfil e nome do post
     divInformacaoDoUsuario.className = "usuario_post" //estilo da div
@@ -30,27 +34,33 @@ function publicarPost(){
     // criando a foto do usuario no post
     const fotoDoUsuario = document.createElement("img") // criando img
     fotoDoUsuario.className = "img_post" // estilo da img
-    fotoDoUsuario.src ="https://avatars.githubusercontent.com/u/63205222?v=4"
+    fotoDoUsuario.src = "https://avatars.githubusercontent.com/u/63205222?v=4"
     fotoDoUsuario.alt = "Erika Marques"
 
     // criando nome do usuario
-    const nomeUsuario = document.createElement("h3") 
+    const nomeUsuario = document.createElement("h3")
     nomeUsuario.innerText = "Erika Marques"
 
     // criando paragrafo do post
-    const paragrafo = document.createElement("p") 
+    const paragrafo = document.createElement("p")
     paragrafo.className = "texto_publicacao" // associando o estilo css para a tag crianda
     paragrafo.innerText = conteudoPost // colocando o conteudo que conseguimos acessar da modal
     elementoPost.textContent = null // limpando o texto da modal apos a publicaçao,para as futuras publicaçoes a modal estar sem nenhum texto
 
     //criando a img do post 
-    const fotoDoPost = document.createElement("img") // criando img
-    fotoDoPost.className = "publicacao" // estilo da img
-    fotoDoPost.src ="assets/images/casas/apartamento-alugar.png"
-    fotoDoPost.alt = "apartamento para alugar"
+    let fotosDoPost = [];
+    for (let i = 0; i < imgPostModal.children.length; i++) {
+        let imgPost = imgPostModal.children[i]
+        let fotoDoPost = document.createElement("img") // criando img
+        fotoDoPost.className = "publicacao" // estilo da img
+        fotoDoPost.src = imgPost.src
+        fotosDoPost.push(fotoDoPost)
+    }
+
+    // Removendo elementos img da modal
+    imgPostModal.textContent = null;
 
     // criando area de comentarios 
-    
     //div
     const divComentarios = document.createElement("div");
     divComentarios.className = "area_comentarios"; // estilo
@@ -74,8 +84,8 @@ function publicarPost(){
     divComentarios.appendChild(UsuarioComentarioImg);
     divComentarios.appendChild(comentarioUsuario);
     criandoDiv.prepend(divInformacaoDoUsuario); // prepend para ele ser sempre o que veem em primeiro no post
-    criandoDiv.append(paragrafo); 
-    criandoDiv.append(fotoDoPost);
+    criandoDiv.append(paragrafo);
+    fotosDoPost.forEach(imgTags => criandoDiv.append(imgTags));
     criandoDiv.append(divComentarios);
 
     recuperarSessao.prepend(criandoDiv) // jogando a div que criamos dentro da sessao, para isso associamos a div como filho da sessao
@@ -106,13 +116,13 @@ function checkLogin(form) {
         }
 
         if (form.username.value === username && form.password.value === password) {
-            localStorage.setItem('loggedIn', form.username.value)
+            userSession(form.username.value, usuarioCadastrado);
             location.reload();
         } else {
             alert('Please check your username/password')
         }
     } else {
-        alert('Please register first')
+        alert('Por favor se cadastre')
     }
 }
 /*Login / Cadastro*/
@@ -140,13 +150,15 @@ function addUser(id, username, email, password) {
  */
 function register(form) {
 
+    if (form.username.value === '' || form.password.value === '') {
+        return;
+    }
+
     const newUser = addUser(uuid(), form.username.value, form.email.value, form.password.value);
 
     if (localStorage.getItem('users')) {
         let storedUsers = JSON.parse(localStorage.getItem('users'));
-
         storedUsers.push(newUser)
-
         localStorage.setItem('users', JSON.stringify(storedUsers));
     } else {
         let users = [];
@@ -154,9 +166,47 @@ function register(form) {
         localStorage.setItem('users', JSON.stringify(users));
     }
 
-    localStorage.setItem('loggedIn', 'yes');
+    userSession(newUser, novoUsuario);
 }
 
+function userSession(userParam, tipoUsuario) {
+
+    if (tipoUsuario === novoUsuario) {
+        sessionStorage.setItem("loggedUser", JSON.stringify(userParam))
+    }
+    else {
+        const users = JSON.parse(localStorage.getItem('users'))
+        for (let user of users) {
+            if (user.email === userParam) {
+                sessionStorage.setItem("loggedUser", JSON.stringify(user))
+            }
+        }
+    }
+}
+
+
+function addImgToPost(inputElement) {
+
+    // Buscando sessao das imagens
+    const imgPostSection = document.getElementById("img-modal-post")
+    const files = Array.from(inputElement.files);
+
+    files.forEach(file => {
+
+        // Criando URL
+        const imgUrl = URL.createObjectURL(file)
+        // Criando elemento img e atribuindo link da imagem
+        const imgPost = document.createElement("img")
+        imgPost.src = imgUrl;
+
+        // Adicionando novo elemento a sessao
+        imgPostSection.appendChild(imgPost)
+    })
+
+
+    //Limpando input
+    inputElement.value = null;
+}
 
 /* Utils */
 

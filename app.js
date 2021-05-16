76978f6a5282f834a7fde95ae97c5b7c6a2f9060
Juan Marques <juan.marques@docshifter.com>
2021-05-17 00:02:25 +0200

    Adicionando menu dropdown

--- app.js
+++ app.js
@@ -130,6 +130,9 @@ function addUser(id, name, email, password, birthday, postalCode, neighborhood) 
     };
 }
 
+/** Const pra identificar se e um usuario novo ou ja existente */
+const novoUsuario = 1;
+const usuarioCadastrado = 2;
 
 /**
  * Cadastra usuario e ativa sessao.
@@ -205,70 +208,6 @@ function addImgToPost(inputElement) {
 
 /* Utils */
 
-const novoUsuario = 1;
-const usuarioCadastrado = 2;
-
-function buscarBairros(elem) {
-    console.log(elem);
-    /* // Check if input is empty
-     if (elem.value.trim() !== "") {
-         elem.classList.add("dropdown"); // Add dropdown class (for the CSS border-radius)
-         // If the selector div element does not exist, create it
-         if (selector == null) {
-             selector = document.createElement("div");
-             selector.id = "selector";
-             elem.parentNode.appendChild(selector);
-             // Position it below the input element
-             selector.style.left = elem.getBoundingClientRect().left + "px";
-             selector.style.top = elem.getBoundingClientRect().bottom + "px";
-             selector.style.width = elem.getBoundingClientRect().width + "px";
-         }
-         // Clear everything before new search
-         selector.innerHTML = "";
-         // Variable if result is empty
-         let empty = true;
-         for (let item in db) {
-             // Join the db elements in one string
-             let str = [item.toLowerCase(), db[item][0].toLowerCase(), db[item][1].toLowerCase()].join();
-             // If exists, create an item (button)
-             if (str.indexOf(elem.value) !== -1) {
-                 let opt = document.createElement("button");
-                 opt.setAttribute("onclick","insertValue(this);")
-                 opt.innerHTML = db[item][0];
-                 selector.appendChild(opt);
-                 empty = false;
-             }
-         }
-         // If result is empty, display a disabled button with text
-         if (empty === true) {
-             let opt = document.createElement("button");
-             opt.disabled = true;
-             opt.innerHTML = "No results";
-             selector.appendChild(opt);
-         }
-     }
-     // Remove selector element if input is empty
-     else {
-         if (selector !== null) {
-             selector.parentNode.removeChild(selector);
-             elem.classList.remove("dropdown");
-         }
-     }*/
-}
-
-function insertValue(elem) {
-    window.search.classList.remove("dropdown");
-    window.search.value = elem.innerHTML;
-    elem.parentNode.parentNode.removeChild(elem.parentNode);
-}
-
-const db = {
-    "03067": ["Bake Rolls 100g", "snack"],
-    "04089": ["Potato Chips 70g", "snack"],
-    "05612": ["Ice Coffee 100ml", "drink"],
-    "07740": ["Sparkling Water 1.5l", "drink"]
-}
-
 function uuid() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
@@ -331,6 +270,24 @@ function procurarMenuNavAtivo() {
                 return menuNavLi.children[i].dataset.tipo;
             }
         }
+    }
+}
+
+/**
+ * Adiciona class permahover para exibir a modal e so desaparecer quando clicar novamente.
+ * @param {} elemento li dos icons do header
+ */
+function adicionaPermaHoverClass(elemento) {
+
+    switch (elemento.dataset.tipo) {
+        case 'notifications':
+            elemento.classList.toggle("permahover-notifications");
+            document.getElementById("profile-icon").classList.remove("permahover-profile");
+            break;
+        case 'profile':
+            elemento.classList.toggle("permahover-profile");
+            document.getElementById("notifications-icon").classList.remove("permahover-notifications")
+            break;
     }
 }
 
